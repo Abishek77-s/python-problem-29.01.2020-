0c81cba026d570890606114c96361174650170e9
Abishek77-s <60419027+Abishek77-s@users.noreply.github.com>
2020-01-29 12:24:40 +0530

    Create numberseries.py

---
+++ numberseries.py
@@ -0,0 +1,24 @@
+print ("serie of numbers from 100 to 500:\n")
+for i in range(100,500):
+  print(i)
+
+sample output:
+series of numbers from 100 to 500:
+100
+101
+102
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+499
